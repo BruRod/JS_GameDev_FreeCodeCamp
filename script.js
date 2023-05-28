fcb923859b480c56f95a3e8e7f29c1c08ba4bb1c
BruRod <brunorodrigues80@yahoo.de>
2023-05-28 21:18:29 +0100

    Particles

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ window.addEventListener("load", function(){
 
     ctx.fillStyle = 'white';
     ctx.lineWidth = 3;
-    ctx.strokeStyle = 'white';
+    ctx.strokeStyle = 'black';
     ctx.font = '40px Helvetica';
     ctx.textAlign = 'center';
 
@@ -248,6 +248,9 @@ window.addEventListener("load", function(){
                 this.markedForDeletion = true;
                 this.game.removeGameObjects();
                 this.game.score++;
+                for (let i=0; i < 3; i++){
+                    this.game.particles.push(new FireFly(this.game, this.collisionX, this.collisionY, 'yellow'));
+                }
             }
             //collisions with objects
             let collisionObjects = [this.game.player, ...this.game.obstacles];
@@ -266,6 +269,9 @@ window.addEventListener("load", function(){
                     this.markedForDeletion = true;
                     this.game.removeGameObjects();
                     this.game.lostHatchlings ++;
+                    for (let i=0; i < 3; i++){
+                        this.game.particles.push(new Spark(this.game, this.collisionX, this.collisionY, 'blue'));
+                    }
                 }
             });
         }
@@ -321,6 +327,50 @@ window.addEventListener("load", function(){
         }
     }
 
+    class Particle {
+        constructor(game, x, y, color){
+            this.game = game;
+            this.collisionX = x;
+            this.collisionY = y;
+            this.color = color;
+            this.radius = Math.floor( Math.random() * 10 + 5);
+            this.speedX = Math.random() * 6 - 3;
+            this.speedY = Math.random() * 2 + 0.5;
+            this.angle = 0;
+            this.va = Math.random() * 0.1 + 0.01;
+            this.markedForDeletion = false;
+        }
+        draw(context){
+            context.save();
+            context.fillStyle = this.color;
+            context.beginPath();
+            context.arc(this.collisionX, this.collisionY, this.radius, 0, Math.PI * 2 );
+            context.fill();
+            context.stroke();
+            context.restore();
+        }
+    }
+
+    class FireFly extends Particle {
+         update(){
+            this.angle += this.va;
+            this.collisionX += Math.cos(this.angle) * this.speedX;
+            this.collisionY -= this.speedY;
+            if (this.collisionY < 0 - this.radius){
+                this.markedForDeletion = true;
+                this.game.removeGameObjects();
+             }
+         }
+    }
+
+    class Spark extends Particle {
+        update(){
+            this.angle += this.va * 0.5;
+            this.collisionX -= Math.cos(this.angle) * this.speedX;
+            this.collisionY -= Math.sin(this.angle) * this.speedY;
+        }
+    }
+
     class Game {
         constructor(canvas){
             this.canvas = canvas;
@@ -337,6 +387,7 @@ window.addEventListener("load", function(){
             this.eggs = [];
             this.enemies = [];
             this.hatchlings = [];
+            this.particles = [];
             this.score = 0;
             this.lostHatchlings = 0;
             this.gameObjects = [];
@@ -383,11 +434,12 @@ window.addEventListener("load", function(){
         removeGameObjects(){
             this.eggs = this.eggs.filter( object => !object.markedForDeletion);
             this.hatchlings = this.hatchlings.filter( object => !object.markedForDeletion);
+            this.particles = this.particles.filter( object => !object.markedForDeletion);
         }
            
 
         render(context, deltaTime){
-            this.gameObjects = [...this.eggs, ...this.obstacles, this.player, ...this.enemies, ...this.hatchlings];
+            this.gameObjects = [...this.eggs, ...this.obstacles, this.player, ...this.enemies, ...this.hatchlings, ...this.particles];
             //sort objects by vertical position
             this.gameObjects.sort((a, b) => {
                 return a.collisionY - b.collisionY;
